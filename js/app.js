f43317b6410925239903833863c45df6e7f395d3
Albiddd <alberto.dore93@gmail.com>
2022-07-21 17:58:39 +0200

    add results

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const buttonElement = document.querySelector('input[type="submit"]');
 buttonElement.addEventListener('click', function(){  
     const km = document.querySelector('input[name="km"]');
     const eta = document.querySelector('#eta').value;
+    const nome = document.querySelector('#nome').value;
 
     const prezzo = parseFloat(km.value) * 0.21;
     let percentualeSconto = 0;
@@ -10,21 +11,17 @@ buttonElement.addEventListener('click', function(){
     const carrozza = document.getElementById('carrozza');
     const costo = document.getElementById('costo');
 
+    passeggero.innerHTML = nome
     const biglietto = document.getElementById('row-biglietto');
     biglietto.style.display = 'flex'
 
-    
-    
-    if (eta <= 0 || eta >= 130){
-        alert('ATTENZIONE! Il valore inserito per l`età non è valido.');
-        // biglietto.innerHTML = 'Nessuno biglietto generato.';
-    }
-    else if (eta < 18){
+
+   if (eta < 18){
         let percentualeSconto = 0.2;
         const sconto = prezzo * percentualeSconto;
         let prezzoScontato = prezzo - sconto;
         prezzoScontato = prezzoScontato.toFixed(2);
-        // biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzoScontato + '&euro;';
+        costo.innerHTML = prezzoScontato + '&euro;';
 
     }
     else if (eta >= 65){
@@ -32,13 +29,13 @@ buttonElement.addEventListener('click', function(){
         const sconto = prezzo * percentualeSconto;
         let prezzoScontato = prezzo - sconto;
         prezzoScontato = prezzoScontato.toFixed(2);
-        // biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzoScontato + '&euro;';
+        costo.innerHTML = prezzoScontato + '&euro;';
     }
     else{
         const sconto = prezzo * percentualeSconto;
         let prezzoScontato = prezzo - sconto;
         prezzoScontato = prezzoScontato.toFixed(2);
-        // biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzo + '&euro;';
+        costo.innerHTML = prezzo + '&euro;';;
         
     }
 } )
