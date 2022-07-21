fd5270431722e992d79243f11a67a0260daacfb1
Albiddd <alberto.dore93@gmail.com>
2022-07-21 17:51:25 +0200

    style calcolo

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
 const buttonElement = document.querySelector('input[type="submit"]');
-buttonElement.addEventListener('click', function(){
+buttonElement.addEventListener('click', function(){  
     const km = document.querySelector('input[name="km"]');
-    const eta = document.querySelector('input[name="eta"]').value;
+    const eta = document.querySelector('#eta').value;
 
     const prezzo = parseFloat(km.value) * 0.21;
-    const biglietto = document.getElementById('biglietto');
     let percentualeSconto = 0;
+    const passeggero = document.getElementById('passeggero');
+    const offerta = document.getElementById('offerta');
+    const carrozza = document.getElementById('carrozza');
+    const costo = document.getElementById('costo');
+
+    const biglietto = document.getElementById('row-biglietto');
+    biglietto.style.display = 'flex'
+
+    
     
     if (eta <= 0 || eta >= 130){
         alert('ATTENZIONE! Il valore inserito per l`età non è valido.');
-        biglietto.innerHTML = 'Nessuno biglietto generato.';
+        // biglietto.innerHTML = 'Nessuno biglietto generato.';
     }
     else if (eta < 18){
         let percentualeSconto = 0.2;
         const sconto = prezzo * percentualeSconto;
         let prezzoScontato = prezzo - sconto;
         prezzoScontato = prezzoScontato.toFixed(2);
-        biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzoScontato + '&euro;';
+        // biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzoScontato + '&euro;';
 
     }
     else if (eta >= 65){
@@ -24,13 +32,13 @@ buttonElement.addEventListener('click', function(){
         const sconto = prezzo * percentualeSconto;
         let prezzoScontato = prezzo - sconto;
         prezzoScontato = prezzoScontato.toFixed(2);
-        biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzoScontato + '&euro;';
+        // biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzoScontato + '&euro;';
     }
     else{
         const sconto = prezzo * percentualeSconto;
         let prezzoScontato = prezzo - sconto;
         prezzoScontato = prezzoScontato.toFixed(2);
-        biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzo + '&euro;';
+        // biglietto.innerHTML = 'Il tuo biglietto costa: ' + prezzo + '&euro;';
         
     }
 } )
